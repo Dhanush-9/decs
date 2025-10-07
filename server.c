2216a3d6266f39c705c634c7d3705174d3908a4e
Dhanush-9 <dhanushsonkusare31@gmail.com>
2025-10-07 11:37:16 +0530

    Added thread implementation in server

--- server.c
+++ server.c
@@ -7,9 +7,13 @@
 #include <string.h>
 #include <netdb.h>
 #include <arpa/inet.h>
+#include <pthread.h>
+#include <semaphore.h>
 
 #define TABLE_SIZE 1024
 #define MAX_BUFFER_SIZE 256
+
+sem_t mutex;
 
 void error(char *msg)
 {
@@ -138,8 +142,253 @@ int delete(KeyValue **table, int key)
     return 0;
 }
 
+void* handle_client(void *newsockfd){
+    int n;
+    char buffer[MAX_BUFFER_SIZE];
+    int sock = *(int *)newsockfd;
+
+    while (1)
+    {
+        bzero(buffer, MAX_BUFFER_SIZE);
+        int n;
+        n = read(sock, buffer, MAX_BUFFER_SIZE - 1);
+        if (n < 0)
+        {
+            error("ERROR reading from socket");
+        }
+        else if (n == 0)
+        {
+            printf("> Client disconnected.\n");
+            close(sock);
+            break;
+        }
+        buffer[n] = '\0';
+
+        char *command = strtok(buffer, " ");
+        printf("Command : %s", command);
+
+        if (strcmp(command, "create") == 0)
+        {
+            n = read(sock, buffer, MAX_BUFFER_SIZE - 1);
+            if (n < 0)
+            {
+                error("ERROR reading from socket");
+            }
+            buffer[n] = '\0';
+            int key = atoi(buffer);
+
+            n = read(sock, buffer, MAX_BUFFER_SIZE - 1);
+            if (n < 0)
+            {
+                error("ERROR reading from socket");
+            }
+            int value_size = atoi(buffer);
+
+            int received_bytes = 0;
+            char *value = (char *)malloc(value_size + 1);
+
+            while (received_bytes < value_size)
+            {
+                int len_to_recieve;
+                if (value_size - received_bytes > MAX_BUFFER_SIZE - 1)
+                {
+                    len_to_recieve = MAX_BUFFER_SIZE - 1;
+                }
+                else
+                {
+                    len_to_recieve = value_size - received_bytes;
+                }
+
+                n = read(sock, value + received_bytes, len_to_recieve);
+                if (n < 0)
+                {
+                    error("ERROR reading from socket");
+                }
+                received_bytes += n;
+            }
+
+            value[value_size] = '\0';
+
+            char *response;
+            sem_wait(&mutex);
+            if (search(table, key) != NULL)
+            {
+                response = "Error: Key already exists";
+            }
+            else
+            {
+                insert(table, key, value);
+                response = "Key-Value pair created successfully";
+            }
+            sem_post(&mutex);
+
+            n = write(sock, response, strlen(response) + 1);
+            if (n < 0)
+            {
+                error("ERROR writing to socket");
+            }
+
+            printf(" (Key: %d, Value: %s)\n", key, value);
+            free(value);
+        }
+
+        else if(strcmp(command, "read") == 0)
+        {
+            n = read(sock, buffer, MAX_BUFFER_SIZE - 1);
+            if (n < 0)
+            {
+                error("ERROR reading from socket");
+            }
+            buffer[n] = '\0';
+            int key = atoi(buffer);
+
+            printf(" (Key: %d)\n", key);
+            sem_wait(&mutex);
+            char *value = search(table, key);
+            sem_post(&mutex);
+            if (value == NULL){
+                char *response = "ERROR Error: Key not found";
+                bzero(buffer, MAX_BUFFER_SIZE);
+                strcpy(buffer, response);
+
+                n = write(sock, buffer, MAX_BUFFER_SIZE-1);
+                if(n < 0){
+                    error("ERROR writing to socket");
+                }
+            }
+            else{
+                int value_size = strlen(value);
+                bzero(buffer, MAX_BUFFER_SIZE);
+                snprintf(buffer, MAX_BUFFER_SIZE, "OK %d", value_size);
+
+                n = write(sock, buffer, MAX_BUFFER_SIZE-1);
+                if(n < 0){
+                    error("ERROR writing to socket");
+                }
+
+                int sent_bytes = 0;
+
+                while(sent_bytes < value_size){
+                    int len_to_send;
+                    if(value_size - sent_bytes > MAX_BUFFER_SIZE - 1){
+                        len_to_send = MAX_BUFFER_SIZE - 1;
+                    }
+                    else{
+                        len_to_send = value_size - sent_bytes;
+                    }
+
+                    n = write(sock, value + sent_bytes, len_to_send);
+                    if(n < 0){
+                        error("ERROR writing to socket");
+                    }
+                    sent_bytes += n;
+                }
+            }
+        }
+
+        else if(strcmp(command, "update") == 0)
+        {
+            n = read(sock, buffer, MAX_BUFFER_SIZE - 1);
+            if (n < 0)
+            {
+                error("ERROR reading from socket");
+            }
+            buffer[n] = '\0';
+            int key = atoi(buffer);
+
+            n = read(sock, buffer, MAX_BUFFER_SIZE - 1);
+            if (n < 0)
+            {
+                error("ERROR reading from socket");
+            }
+            int value_size = atoi(buffer);
+
+            int received_bytes = 0;
+            char *newValue = (char *)malloc(value_size + 1);
+
+            while (received_bytes < value_size)
+            {
+                int len_to_recieve;
+                if (value_size - received_bytes > MAX_BUFFER_SIZE - 1)
+                {
+                    len_to_recieve = MAX_BUFFER_SIZE - 1;
+                }
+                else
+                {
+                    len_to_recieve = value_size - received_bytes;
+                }
+
+                n = read(sock, newValue + received_bytes, len_to_recieve);
+                if (n < 0)
+                {
+                    error("ERROR reading from socket");
+                }
+                received_bytes += n;
+            }
+
+            newValue[value_size] = '\0';
+
+            char *response;
+            sem_wait(&mutex);
+            if (update(table, key, newValue))
+            {
+                response = "Key-Value pair updated successfully";
+            }
+            else
+            {
+                response = "Error: Key not found";
+            }
+            sem_post(&mutex);
+            bzero(buffer, MAX_BUFFER_SIZE);
+            strcpy(buffer, response);
+            n = write(sock, buffer, MAX_BUFFER_SIZE - 1);
+            if (n < 0)
+            {
+                error("ERROR writing to socket");
+            }
+
+            printf(" (Key: %d, New Value: %s)\n", key, newValue);
+            free(newValue);
+        }
+
+        else if (strcmp(command, "delete") == 0)
+        {
+            n = read(sock, buffer, MAX_BUFFER_SIZE - 1);
+            if (n < 0)
+            {
+                error("ERROR reading from socket");
+            }
+            buffer[n] = '\0';
+            int key = atoi(buffer);
+
+            char *response;
+            sem_wait(&mutex);
+            if (delete(table, key))
+            {
+                response = "Key-Value pair deleted successfully";
+            }
+            else
+            {
+                response = "Error: Key not found";
+            }
+            sem_post(&mutex);
+            bzero(buffer, MAX_BUFFER_SIZE);
+            strcpy(buffer, response);
+            n = write(sock, buffer, MAX_BUFFER_SIZE - 1 );
+            if (n < 0)
+            {
+                error("ERROR writing to socket");
+            }
+
+            printf(" (Key: %d)\n", key);
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
+
+    sem_init(&mutex, 0, 1);
     int sockfd, newsockfd, portno, clilen;
     char buffer[MAX_BUFFER_SIZE];
 
@@ -200,235 +449,9 @@ int main(int argc, char *argv[])
 
         printf("> Client with IP address %s and port %d connected.\n", inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
 
-        while (1)
-        {
-            bzero(buffer, MAX_BUFFER_SIZE);
-            int n;
-            n = read(newsockfd, buffer, MAX_BUFFER_SIZE - 1);
-            if (n < 0)
-            {
-                error("ERROR reading from socket");
-            }
-            else if (n == 0)
-            {
-                printf("> Client with IP address %s and port %d disconnected.\n", inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
-                close(newsockfd);
-                break;
-            }
-            buffer[n] = '\0';
-
-            char *command = strtok(buffer, " ");
-            printf("Command : %s", command);
-
-            if (strcmp(command, "create") == 0)
-            {
-                n = read(newsockfd, buffer, MAX_BUFFER_SIZE - 1);
-                if (n < 0)
-                {
-                    error("ERROR reading from socket");
-                }
-                buffer[n] = '\0';
-                int key = atoi(buffer);
-
-                n = read(newsockfd, buffer, MAX_BUFFER_SIZE - 1);
-                if (n < 0)
-                {
-                    error("ERROR reading from socket");
-                }
-                int value_size = atoi(buffer);
-
-                int received_bytes = 0;
-                char *value = (char *)malloc(value_size + 1);
-
-                while (received_bytes < value_size)
-                {
-                    int len_to_recieve;
-                    if (value_size - received_bytes > MAX_BUFFER_SIZE - 1)
-                    {
-                        len_to_recieve = MAX_BUFFER_SIZE - 1;
-                    }
-                    else
-                    {
-                        len_to_recieve = value_size - received_bytes;
-                    }
-
-                    n = read(newsockfd, value + received_bytes, len_to_recieve);
-                    if (n < 0)
-                    {
-                        error("ERROR reading from socket");
-                    }
-                    received_bytes += n;
-                }
-
-                value[value_size] = '\0';
-
-                char *response;
-                if (search(table, key) != NULL)
-                {
-                    response = "Error: Key already exists";
-                }
-                else
-                {
-                    insert(table, key, value);
-                    response = "Key-Value pair created successfully";
-                }
-
-                n = write(newsockfd, response, strlen(response) + 1);
-                if (n < 0)
-                {
-                    error("ERROR writing to socket");
-                }
-
-                printf(" (Key: %d, Value: %s)\n", key, value);
-                free(value);
-            }
-
-            else if(strcmp(command, "read") == 0)
-            {
-                n = read(newsockfd, buffer, MAX_BUFFER_SIZE - 1);
-                if (n < 0)
-                {
-                    error("ERROR reading from socket");
-                }
-                buffer[n] = '\0';
-                int key = atoi(buffer);
-
-                printf(" (Key: %d)\n", key);
-
-                char *value = search(table, key);
-                if (value == NULL){
-                    char *response = "ERROR Error: Key not found";
-                    bzero(buffer, MAX_BUFFER_SIZE);
-                    strcpy(buffer, response);
-
-                    n = write(newsockfd, buffer, MAX_BUFFER_SIZE-1);
-                    if(n < 0){
-                        error("ERROR writing to socket");
-                    }
-                }
-                else{
-                    int value_size = strlen(value);
-                    bzero(buffer, MAX_BUFFER_SIZE);
-                    snprintf(buffer, MAX_BUFFER_SIZE, "OK %d", value_size);
-
-                    n = write(newsockfd, buffer, MAX_BUFFER_SIZE-1);
-                    if(n < 0){
-                        error("ERROR writing to socket");
-                    }
-
-                    int sent_bytes = 0;
-
-                    while(sent_bytes < value_size){
-                        int len_to_send;
-                        if(value_size - sent_bytes > MAX_BUFFER_SIZE - 1){
-                            len_to_send = MAX_BUFFER_SIZE - 1;
-                        }
-                        else{
-                            len_to_send = value_size - sent_bytes;
-                        }
-
-                        n = write(newsockfd, value + sent_bytes, len_to_send);
-                        if(n < 0){
-                            error("ERROR writing to socket");
-                        }
-                        sent_bytes += n;
-                    }
-                }
-            }
-
-            else if(strcmp(command, "update") == 0)
-            {
-                n = read(newsockfd, buffer, MAX_BUFFER_SIZE - 1);
-                if (n < 0)
-                {
-                    error("ERROR reading from socket");
-                }
-                buffer[n] = '\0';
-                int key = atoi(buffer);
-
-                n = read(newsockfd, buffer, MAX_BUFFER_SIZE - 1);
-                if (n < 0)
-                {
-                    error("ERROR reading from socket");
-                }
-                int value_size = atoi(buffer);
-
-                int received_bytes = 0;
-                char *newValue = (char *)malloc(value_size + 1);
-
-                while (received_bytes < value_size)
-                {
-                    int len_to_recieve;
-                    if (value_size - received_bytes > MAX_BUFFER_SIZE - 1)
-                    {
-                        len_to_recieve = MAX_BUFFER_SIZE - 1;
-                    }
-                    else
-                    {
-                        len_to_recieve = value_size - received_bytes;
-                    }
-
-                    n = read(newsockfd, newValue + received_bytes, len_to_recieve);
-                    if (n < 0)
-                    {
-                        error("ERROR reading from socket");
-                    }
-                    received_bytes += n;
-                }
-
-                newValue[value_size] = '\0';
-
-                char *response;
-                if (update(table, key, newValue))
-                {
-                    response = "Key-Value pair updated successfully";
-                }
-                else
-                {
-                    response = "Error: Key not found";
-                }
-                bzero(buffer, MAX_BUFFER_SIZE);
-                strcpy(buffer, response);
-                n = write(newsockfd, buffer, MAX_BUFFER_SIZE - 1);
-                if (n < 0)
-                {
-                    error("ERROR writing to socket");
-                }
-
-                printf(" (Key: %d, New Value: %s)\n", key, newValue);
-                free(newValue);
-            }
-
-            else if (strcmp(command, "delete") == 0)
-            {
-                n = read(newsockfd, buffer, MAX_BUFFER_SIZE - 1);
-                if (n < 0)
-                {
-                    error("ERROR reading from socket");
-                }
-                buffer[n] = '\0';
-                int key = atoi(buffer);
-
-                char *response;
-                if (delete(table, key))
-                {
-                    response = "Key-Value pair deleted successfully";
-                }
-                else
-                {
-                    response = "Error: Key not found";
-                }
-                bzero(buffer, MAX_BUFFER_SIZE);
-                strcpy(buffer, response);
-                n = write(newsockfd, buffer, MAX_BUFFER_SIZE - 1 );
-                if (n < 0)
-                {
-                    error("ERROR writing to socket");
-                }
-
-                printf(" (Key: %d)\n", key);
-            }
-        }
+        pthread_t thread_id;
+        pthread_create(&thread_id, NULL, handle_client, &newsockfd);
+        
     }
     return 0;
 }
